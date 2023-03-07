6dcba68e9e0d317895c16ea41a108f4140adfbdb
Natalia Percario <nataliapercario@gmail.com>
2023-03-07 17:18:08 -0300

    agrego product card

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import ItemListContainer from "./Components/ItemListContainer/ItemListContainer";
 import Navbar from "./Components/Navbar/Navbar";
+import ProductCard from "./Components/ProductCard/ProductCard";
 
 function App() {
   return (
     <div>
       <Navbar />
       <ItemListContainer greeting={"Bienvenidos a Macreame"} />
+      <ProductCard title={"Producto uno"} price={200} isRed={false} />
     </div>
   );
 }
